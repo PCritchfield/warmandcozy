4929bf6dcea94d687c908cd211150ce9fe3176fc
Phil Critchfield <pjcritchfield@gmail.com>
2023-11-29 08:51:30 -0500

    testing the vpc to determine why ECS won't launch

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,45 +1,49 @@
 import * as aws from "@pulumi/aws";
-import { SecurityGroup } from "@pulumi/aws/ec2";
 import * as awsx from "@pulumi/awsx";
 
 // Set the names of your resources
-let vpcName = "myVPC";
-let clusterName = "myCluster";
-let sgName = "mySG"
+const vpcName = "myVPC";
+const clusterName = "myCluster";
+const sgName = "mySG";
+const appName = "warmandcozy"; // Giving a clear name to the app for reference
 
 // Create a new VPC
 const vpc = new awsx.ec2.Vpc(vpcName, {});
 
-// Create an ECS cluster using Fargate
-const cluster = new aws.ecs.Cluster(clusterName, {
-    
-});
-let secGroup = new aws.ec2.SecurityGroup(sgName, {
+// // Create an ECS cluster using Fargate
+// const cluster = new aws.ecs.Cluster(clusterName, {});
+
+// Create a Security Group for the ALB
+const albSecurityGroup = new aws.ec2.SecurityGroup(sgName, {
     vpcId: vpc.vpcId,
+    description: "ALB security group",
     ingress: [
         { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
     ],
 });
 
+// Create an Application Load Balancer
 const alb = new aws.lb.LoadBalancer("app-lb", {
     loadBalancerType: "application",
     subnets: vpc.publicSubnetIds,
-    securityGroups: [secGroup.id],
+    securityGroups: [albSecurityGroup.id],
 });
 
+// Create a Security Group for the ECS Tasks
 const ecsSecurityGroup = new aws.ec2.SecurityGroup("ecsSecurityGroup", {
     vpcId: vpc.vpcId,
+    description: "ECS security group",
     ingress: [
         {
             protocol: "tcp",
-            fromPort: 3000, // The port your container is listening on
+            fromPort: 3000,
             toPort: 3000,
-            securityGroups: [alb.securityGroups[0]], // Allow traffic from the ALB's security group
+            securityGroups: [albSecurityGroup.id], // Use the ALB security group
         },
     ],
     egress: [
         {
-            protocol: "-1", // Allow all outbound traffic
+            protocol: "-1",
             fromPort: 0,
             toPort: 0,
             cidrBlocks: ["0.0.0.0/0"],
@@ -47,62 +51,64 @@ const ecsSecurityGroup = new aws.ec2.SecurityGroup("ecsSecurityGroup", {
     ],
 });
 
-const targetGroup = new aws.lb.TargetGroup("app-tg", {
-    port: 3000,
-    protocol: "HTTP",
-    targetType: "ip",
-    vpcId: vpc.vpcId,
-});
+// // Create a Target Group for the Load Balancer
+// const targetGroup = new aws.lb.TargetGroup("app-tg", {
+//     port: 3000,
+//     protocol: "HTTP",
+//     targetType: "ip",
+//     vpcId: vpc.vpcId,
+// });
 
-const httpListener = new aws.lb.Listener("http-listener", {
-    loadBalancerArn: alb.arn,
-    port: 80,
-    defaultActions: [{
-        type: "forward",
-        targetGroupArn: targetGroup.arn,
-    }],
-});
+// // Create a Listener for the Load Balancer
+// const httpListener = new aws.lb.Listener("http-listener", {
+//     loadBalancerArn: alb.arn,
+//     port: 80,
+//     defaultActions: [{ type: "forward", targetGroupArn: targetGroup.arn }],
+// });
 
-// Define the task definition
-const task = new awsx.ecs.FargateTaskDefinition("task", {
-    executionRole:{
-        skip: true
-    },
-    taskRole:{
-        skip: true
-    },
-    containers: {
-        app: {
-            name: "latest",
-            image: "philjim/warmandcozy:latest",
-            memory: 512,
-            portMappings: [{
-                containerPort: 3000,
-                name: "3000-tcp",
-                protocol: "HTTP"
-              }],
-        },
-    },
-});
+// // Define the task definition
+// const task = new awsx.ecs.FargateTaskDefinition("task", {
+//     // Note: For Fargate tasks, 'family' is not needed, and 'cpu' and 'memory' are defined at the container level
+//     containers: {
+//         [appName]: {
+//             name: appName,
+//             image: "philjim/warmandcozy:latest",
+//             memory: 512,
+//             portMappings: [{ containerPort: 3000, protocol: "tcp" }],
+//             logConfiguration: {
+//                 // Ensure you have a log configuration for debugging
+//                 logDriver: "awslogs",
+//                 options: {
+//                     "awslogs-group": "/ecs/app", // Specify your CloudWatch Logs group
+//                     "awslogs-region": "us-west-2", // Specify your AWS region
+//                     "awslogs-stream-prefix": "ecs", // A prefix for the log streams
+//                 },
+//             },
+//         },
+//     },
+// });
 
-// Define the service
-const service = new awsx.ecs.FargateService("service", {
-    cluster: cluster.arn,
-    taskDefinition: task.taskDefinition.arn,
-    desiredCount: 1,
-    networkConfiguration: {
-        subnets: vpc.publicSubnetIds,
-        securityGroups: [ecsSecurityGroup.id],
-        assignPublicIp: false
-    },
-    loadBalancers: [{
-        targetGroupArn: targetGroup.arn,
-        containerName: "app", // The name of the container in your task definition
-        containerPort: 3000,   // The port on which your container is listening
-    }],
-});
+// // Define the service
+// const service = new awsx.ecs.EC2Service("service", {
+//     cluster: cluster.arn,
+//     taskDefinition: task.taskDefinition.arn,
+//     desiredCount: 1,
+//     networkConfiguration: {
+//         subnets: vpc.publicSubnetIds,
+//         securityGroups: [ecsSecurityGroup.id],
+//     },
+//     loadBalancers: [{
+//         targetGroupArn: targetGroup.arn,
+//         containerName: appName, // match the name defined in the containers map
+//         containerPort: 3000,
+//     }],
+// });
 
-const logGroup = new aws.cloudwatch.LogGroup("LogGroup");
+// // Create a CloudWatch Log Group for the ECS task logs
+// const logGroup = new aws.cloudwatch.LogGroup("LogGroup", {
+//     // It's good practice to specify the retention policy
+//     retentionInDays: 30,
+// });
 
 // Outputs
 export const url = alb.dnsName;
